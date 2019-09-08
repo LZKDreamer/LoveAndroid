d7c84900386285f85623cbc4da795d0604fa1ac1
LZKDreamer <1254233340@qq.com>
2019-09-08 20:35:53 +0800

    完成搜索第一个页面

---
+++ app/src/main/java/com/lzk/loveandroid/Search/SearchListActivity.java
@@ -0,0 +1,16 @@
+package com.lzk.loveandroid.Search;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.lzk.loveandroid.R;
+
+public class SearchListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_list);
+    }
+}
